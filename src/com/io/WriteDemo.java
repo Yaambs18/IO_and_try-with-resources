1cf1f8320e220848f869d7a0864a8b570219028a
yaambs <yanshbhardwaj14@gmail.com>
2021-11-11 15:08:47 +0530

    write method code implemented and added

---
+++ src/com/io/WriteDemo.java
@@ -0,0 +1,10 @@
+package com.io;
+
+public class WriteDemo {
+    public static void main(String[] args) {
+        int asciiValue;
+        asciiValue = 'A';
+        System.out.write(asciiValue);
+        System.out.write('\n');
+    }
+}
