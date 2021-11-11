832eaaad33b2140bc692fa63c427e4a142c2a538
yaambs <yanshbhardwaj14@gmail.com>
2021-11-11 15:09:23 +0530

    Print Writer class code implemented and added

---
+++ src/com/io/PrintWriterDemo.java
@@ -0,0 +1,15 @@
+package com.io;
+
+import java.io.PrintWriter;
+
+public class PrintWriterDemo {
+    public static void main(String[] args) {
+        PrintWriter pw = new PrintWriter(System.out, true);
+
+        pw.println("This is a string");
+        int i = -7;
+        pw.println(i);
+        double d = 4.5;
+        pw.println(d);
+    }
+}
